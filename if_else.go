0ddffef71821a714cd7d30af04e1f61db8d9aead
Ayush0nly <Ayushrahul27@gmail.com>
2026-09-04 11:20:18 +0530

    if_else

---
+++ if_else.go
@@ -0,0 +1,14 @@
+package main 
+import "fmt"
+ 
+func main() {
+	age := 17
+	if age >= 18 {
+		fmt.Println("you can ride!")
+	} else if age>=14{
+		fmt.Println("you can ride with parent!")
+	} else {
+		fmt.Println("you cannot ride!")
+	}
+}
+
